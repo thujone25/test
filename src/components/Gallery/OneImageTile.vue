e6c47c879d0707740c9911dec19bc699f53045a4
St. B <junkbox@email.ua>
2020-07-06 12:37:19 +0300

    Add one image tile skeleton

---
+++ src/components/Gallery/OneImageTile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="one-gallery-image"></div>
+</template>
+
+<style scoped>
+.one-gallery-image {
+  background-color: #ccc;
+  border-radius: 12px;
+  background-image: linear-gradient(90deg, #ddd 0px, #e8e8e8 40px, #ddd 80px);
+  background-size: 600px;
+  animation: shine-avatar 1.6s infinite linear;
+}
+@keyframes shine-avatar {
+  0% {
+    background-position: -32px;
+  }
+  40%, 100% {
+    background-position: 208px;
+  }
+}
+</style>
--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>HEY Gallery!!!</div>
+  <div class="gallery">
+    <OneImage v-for="(item, index) in 25"
+              :key="index"
+              class="gallery__one-image" />
+  </div>
 </template>
+
+<script>
+import OneImage from '@/components/Gallery/OneImageTile';
+
+export default {
+  components: {
+    OneImage
+  }
+}
+</script>
+
+<style scoped>
+.gallery {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  max-width: 1550px;
+  padding: 20px;
+  margin: 0 auto;
+}
+.gallery__one-image {
+  display: block;
+  width: 300px;
+  height: 169px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+</style>
