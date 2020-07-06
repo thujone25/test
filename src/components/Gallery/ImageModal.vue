babcddad001eb85e21cb3cc071c871199e450283
St. B <junkbox@email.ua>
2020-07-06 16:38:02 +0300

    Add prev next btns

--- src/components/Gallery/ImageModal.vue
+++ src/components/Gallery/ImageModal.vue
@@ -3,13 +3,17 @@
        @click="closeModal">
     <div class="image-modal__content"
          @click.stop="">
-      <!-- <div class="image-modal__img"
-           :style="finalStyles"
-           :class="{'image-modal__img--with-image': finalStyles}"></div> -->
       <img :src="img.full_picture"
            :class="{'image-modal__img--with-image': loaded}"
            class="image-modal__img">
-      <p class="image-modal__img-info">{{ imgInfoText }}</p>
+      <p v-if="loaded"
+         class="image-modal__img-info">{{ imgInfoText }}</p>
+      <button v-if="!disablePrev"
+              class="image-modal__prev-img-btn"
+              @click="emitNavigation('prev')"><span>&#x2B05;</span></button>
+      <button v-if="!disableNext"
+              class="image-modal__next-img-btn"
+              @click="emitNavigation('next')"><span>&#x27A1;</span></button>
     </div>
     <button class="image-modal__close-btn">X</button>
   </div>
@@ -19,7 +23,7 @@
 import {mapMutations} from 'vuex'; 
 
 export default {
-  props: ['img'],
+  props: ['img', 'disablePrev', 'disableNext'],
   data() {
     return {
       loaded: false
@@ -35,7 +39,7 @@ export default {
       return undefined;
     },
     imgInfoText() {
-      return [this.img.author || '', this.img.camera || '', this.img.tags || ''].join(', ');
+      return [this.img.author || '', this.img.camera || '', this.img.tags || ''].join(' | ');
     }
   },
   watch: {
@@ -45,14 +49,17 @@ export default {
   },
   methods: {
     ...mapMutations('oneImgStore', ['removeImg']),
+    emitNavigation(type) {
+      this.$emit('catchnav', type);
+    },
     loadPicture() {
       if (this.img.full_picture) {
         let img = new Image();
         img.src = this.img.full_picture;
         img.onload = () => {
           this.loaded = true;
+          img = null;
         };
-        img = null;
       }
     },
     closeModal() {
@@ -104,9 +111,10 @@ export default {
 }
 .image-modal__img-info {
   position: absolute;
-  left: 0;
-  right: 0;
+  left: 40px;
+  right: 40px;
   bottom: 0;
+  z-index: 1;
   padding: 10px;
   font-size: 14px;
   text-shadow: 0 0 3px #000000;
@@ -114,7 +122,33 @@ export default {
   background-color: rgba(10, 10, 10, 0.85);
   opacity: 0.45;
 }
-.image-modal__content:hover .image-modal__img-info {
+.image-modal__prev-img-btn,
+.image-modal__next-img-btn {
+  position: absolute;
+  bottom: 0;
+  top: 0;
+  z-index: 2;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  padding: 10px 0;
+  font-size: 20px;
+  font-weight: bold;
+  text-shadow: 0 0 3px #000000;
+  color: #ffffff;
+  background-color: rgba(10, 10, 10, 0.85);
+  opacity: 0.45;
+}
+.image-modal__prev-img-btn {
+  left: 0;
+}
+.image-modal__next-img-btn {
+  right: 0;
+}
+.image-modal__content:hover .image-modal__img-info,
+.image-modal__content:hover .image-modal__prev-img-btn,
+.image-modal__content:hover .image-modal__next-img-btn {
   opacity: 0.85;
 }
 .image-modal__close-btn {
--- src/pages/Gallery.vue
+++ src/pages/Gallery.vue
@@ -12,7 +12,10 @@
                 @click="loadMorePictures">Load more</button>
       </div>
       <ImageModal v-if="showModal"
-                  :img="image" />
+                  :img="image"
+                  :disable-prev="disablePrev"
+                  :disable-next="disableNext"
+                  @catchnav="catchNav" />
     </template>
     <template v-else>
       <div>Sorry, our database is empty and we don't have any images yet =(</div>
@@ -39,6 +42,12 @@ export default {
   computed: {
     ...mapState('galleryStore', ['images', 'totalPages', 'page']),
     ...mapState('oneImgStore', ['image']),
+    disablePrev() {
+      return this.images.length ? this.$route.query.img == this.images[0].id : true;
+    },
+    disableNext() {
+      return !this.showBtn && (this.images.length ? this.$route.query.img == this.images[this.images.length - 1].id : true);
+    },
     showBtn() {
       return this.page < this.totalPages;
     },
@@ -66,9 +75,27 @@ export default {
     ...mapMutations('oneImgStore', ['removeImg']),
     loadMorePictures() {
       this.disabledBtn = true;
-      this.loadMoreImages().then(() => {
+      return this.loadMoreImages().then(() => {
         this.disabledBtn = false;
       });
+    },
+    findIndexByID(id) {
+      for (let i = 0, lim = this.images.length; i < lim; i += 1) {
+        if (this.images[i].id == id) return i;
+      }
+    },
+    async catchNav(type) {
+      const currentIndex = this.findIndexByID(this.$route.query.img);
+      if (type == 'prev') {
+        const requestedID = this.images[currentIndex - 1].id;
+        this.$router.push({name: this.$route.name, query: {img: requestedID}});
+      } else if (type == 'next') {
+        if (currentIndex == this.images.length - 1) {
+          await this.loadMorePictures();
+        }
+        const requestedID = this.images[currentIndex + 1].id;
+        this.$router.push({name: this.$route.name, query: {img: requestedID}});
+      }
     }
   },
   created() {
